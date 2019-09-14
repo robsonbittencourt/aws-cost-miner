3e6ecb174c29ff6cd2649615ad267e7d97f29446
robsonbittencourt <robson.luizv@gmail.com>
2019-09-14 20:32:03 -0300

    Add project scafold

---
+++ src/main/java/com/rbittencourt/aws/cost/miner/AwsCostMinerApplication.java
@@ -0,0 +1,13 @@
+package com.rbittencourt.aws.cost.miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCostMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCostMinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbittencourt/aws/cost/miner/AwsCostMinerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbittencourt.aws.cost.miner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsCostMinerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
