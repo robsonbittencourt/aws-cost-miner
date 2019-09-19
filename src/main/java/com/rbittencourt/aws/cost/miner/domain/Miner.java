0ef4a74abb60e8a5875aa296f8b17fc108f9db49
robsonbittencourt <robson.luizv@gmail.com>
2019-09-18 21:30:27 -0300

    Add grouping

--- src/main/java/com/rbittencourt/aws/cost/miner/domain/Miner.java
+++ src/main/java/com/rbittencourt/aws/cost/miner/domain/Miner.java
@@ -4,16 +4,20 @@ import com.rbittencourt.aws.cost.miner.domain.billing.BillingInfo;
 import com.rbittencourt.aws.cost.miner.domain.billing.BillingInfoRepository;
 import com.rbittencourt.aws.cost.miner.domain.billing.BillingQuery;
 import com.rbittencourt.aws.cost.miner.domain.metric.Metric;
-import com.rbittencourt.aws.cost.miner.domain.report.Report;
 import com.rbittencourt.aws.cost.miner.domain.report.ec2.Ec2Report;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static java.math.RoundingMode.HALF_EVEN;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.reverseOrder;
 import static org.springframework.util.StringUtils.isEmpty;
 
 @Component
@@ -31,24 +35,48 @@ public class Miner {
     public void miningData() {
         List<BillingInfo> billingInfos = repository.findBillingInfos();
 
-        Predicate<BillingInfo> product = b -> ec2Report.awsProduct().equals(b.getProductName());
-        Predicate<BillingInfo> considerableCost = b -> b.getCost().setScale(2, HALF_EVEN).compareTo(new BigDecimal("0.00")) > 0;
-        List<BillingInfo> filteredBillingInfos = billingQuery.filter(billingInfos, product, considerableCost);
+        List<BillingInfo> filteredBillingInfos = filterBillingInfos(billingInfos);
 
-        writeReport(ec2Report, filteredBillingInfos);
+        Map<String, List<BillingInfo>> groupedBillingInfosOrdered = groupBillingInfos(filteredBillingInfos);
+
+        writeReport(groupedBillingInfosOrdered);
     }
 
-    private void writeReport(Report report, List<BillingInfo> billingInfos) {
-        System.out.println("========== " + report.description() + " ==========" + "\n");
+    private List<BillingInfo> filterBillingInfos(List<BillingInfo> billingInfos) {
+        Predicate<BillingInfo> product = b -> ec2Report.awsProduct().equals(b.getProductName());
+        Predicate<BillingInfo> considerableCost = b -> b.getCost().setScale(2, HALF_EVEN).compareTo(new BigDecimal("0.00")) > 0;
 
-        for (Metric metric : report.metrics()) {
-            if (!isEmpty(metric.description())) {
-                System.out.println(metric.description());
+        return billingQuery.filter(billingInfos, product, considerableCost);
+    }
+
+    private Map<String, List<BillingInfo>> groupBillingInfos(List<BillingInfo> filteredBillingInfos) {
+        Map<String, List<BillingInfo>> groupedBillingInfos = billingQuery.groupBy(filteredBillingInfos, b -> b.getCustomField("user:Name"));
+
+        return groupedBillingInfos.entrySet().stream()
+                .sorted(comparing(v -> billingQuery.totalCost(v.getValue()), reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
+
+    private void writeReport(Map<String, List<BillingInfo>> groupedBillingInfos) {
+        System.out.println("========== " + ec2Report.description() + " ==========" + "\n");
+
+        for (Map.Entry<String, List<BillingInfo>> infos : groupedBillingInfos.entrySet()) {
+            System.out.println(infos.getKey());
+            System.out.println("--------------------------------------");
+
+            for (Metric metric : ec2Report.metrics()) {
+                if (!isEmpty(metric.description())) {
+                    System.out.println(metric.description());
+                }
+
+                String line = metric.calculateMetric(infos.getValue());
+                System.out.println(line);
             }
 
-            String line = metric.calculateMetric(billingInfos);
-            System.out.println(line);
+            System.out.println("");
         }
+
+
     }
 
 }
