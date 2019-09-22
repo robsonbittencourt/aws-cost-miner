9fd4439e5e71562145389eff07fd9a6f252e24d3
robsonbittencourt <robson.luizv@gmail.com>
2019-09-22 20:08:04 -0300

    Separate report build from visualization

--- src/main/java/com/rbittencourt/aws/cost/miner/application/report/ConsoleCostReport.java
+++ src/main/java/com/rbittencourt/aws/cost/miner/application/report/ConsoleCostReport.java
@@ -2,8 +2,6 @@ package com.rbittencourt.aws.cost.miner.application.report;
 
 import com.rbittencourt.aws.cost.miner.domain.awsproduct.AwsProduct;
 import com.rbittencourt.aws.cost.miner.domain.billing.BillingInfo;
-import com.rbittencourt.aws.cost.miner.domain.metric.MetricResult;
-import com.rbittencourt.aws.cost.miner.domain.metric.MetricValue;
 import com.rbittencourt.aws.cost.miner.domain.miner.AwsCostMiner;
 import com.rbittencourt.aws.cost.miner.domain.miner.MinedData;
 import com.rbittencourt.aws.cost.miner.domain.miner.SearchParameters;
@@ -12,18 +10,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.function.Function;
-
-import static java.math.RoundingMode.HALF_EVEN;
-import static org.springframework.util.StringUtils.isEmpty;
 
 @Component
 public class ConsoleCostReport {
 
     @Autowired
     private AwsCostMiner miner;
+
+    @Autowired
+    private ConsoleCostReportPrinter printer;
 
     @Value("${product:#{null}}")
     private String product;
@@ -36,57 +33,7 @@ public class ConsoleCostReport {
         SearchParameters searchParameters = buildSearchParameters(awsProduct);
         List<MinedData> minedData = miner.miningCostData(awsProduct, searchParameters);
 
-        printTitle(awsProduct);
-
-        for (MinedData data : minedData) {
-            BigDecimal totalCost = data.getMetricResult().get(0).getMetricValues().get(0).getValue().setScale(2, HALF_EVEN);
-
-            if (totalCost.compareTo(new BigDecimal("0.00")) > 0) {
-                printHeader(data);
-
-                printMetricResult(data);
-
-                blankLine("");
-            }
-        }
-    }
-
-    private void printTitle(AwsProduct awsProduct) {
-        if (awsProduct != null) {
-            System.out.println("========== Cost Report - " + awsProduct.name() + " ==========" + "\n");
-        } else {
-            System.out.println("========== Cost Report" + " ==========" + "\n");
-        }
-    }
-
-    private void printHeader(MinedData data) {
-        String target = isEmpty(data.getTarget()) ? "Without grouper" : data.getTarget();
-        System.out.println("-----------------------------------------------");
-        System.out.println("| " + target + " |") ;
-        System.out.println("-----------------------------------------------");
-    }
-
-    private void printMetricResult(MinedData data) {
-        for (MetricResult metricResult : data.getMetricResult()) {
-            boolean indent = false;
-            if (metricResult.getDescription().isPresent()) {
-                System.out.println(metricResult.getDescription().get());
-                indent = true;
-            }
-
-            for (MetricValue metricValue : metricResult.getMetricValues()) {
-                if (indent) {
-                    System.out.print("\t");
-                }
-                System.out.println(metricValue);
-            }
-
-            blankLine("");
-        }
-    }
-
-    private void blankLine(String s) {
-        System.out.println(s);
+        printer.printReport(minedData);
     }
 
     private SearchParameters buildSearchParameters(AwsProduct awsProduct) {
