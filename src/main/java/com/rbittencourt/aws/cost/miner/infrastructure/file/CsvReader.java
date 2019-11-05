9fd7b79f14c297855fcf28f053bac458952b2d99
robsonbittencourt <robson@umov.me>
2019-11-05 10:22:45 -0300

    Remove stop condition

--- src/main/java/com/rbittencourt/aws/cost/miner/infrastructure/file/CsvReader.java
+++ src/main/java/com/rbittencourt/aws/cost/miner/infrastructure/file/CsvReader.java
@@ -41,8 +41,6 @@ public class CsvReader {
 
                 if (stopCondition.test(next)) {
                     objects.add(next);
-                } else {
-                    break;
                 }
             }
 
